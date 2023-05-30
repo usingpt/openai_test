b414005953b19deb8167e717f7c9dbfbf7535948
usingpt <134088528+usingpt@users.noreply.github.com>
2023-05-30 20:40:46 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def communicate():
     messages.append(user_message)
 
     response = openai.ChatCompletion.create(
-        model="davinci:ft-personal-2023-05-30-19-10-49",
+        model="gpt-3.5-turbo",
         messages=messages,
 	top_p=1,
 	temperature=1
